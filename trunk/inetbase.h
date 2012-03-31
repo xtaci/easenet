d1b5b4641f447cba6eb78848010f79bb032cab96
skywind3000 <skywind3000@7a243456-3f5e-d0e5-654f-8c31bd46a029>
2012-03-31 07:52:12 +0000

    commit freebsd support

--- trunk/inetbase.h
+++ trunk/inetbase.h
@@ -35,9 +35,14 @@
 #undef __BSD_VISIBLE
 #endif
 
+#ifdef _XOPEN_SOURCE
+#undef _XOPEN_SOURCE
+#endif
+
 #define _GNU_SOURCE 1
 #define _BSD_SOURCE 1
 #define __BSD_VISIBLE 1
+#define _XOPEN_SOURCE 600
 
 
 #include <stddef.h>
